a1cc3251dc37544f9d05297da3c0bddbfb5d5c0e
vamshi_001 <129575030+vamshi-dev-2@users.noreply.github.com>
2025-11-05 19:04:08 +0530

    Remove underline decoration from header text

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
   return (
     <div className="bg-gray-500 place-content-between p-5 flex">
       <div className="flex">
-        <h1 className="text-3xl font-bold text-blue-300 underline decoration-double">
-          Hello {name}😎
+        <h1 className="text-3xl font-bold text-blue-300">
+          Hello {name}
         </h1>
         <StyledLink to="/home">Home</StyledLink>
         {/* <Link className="text-blue-700 mt-2 ml-4" to="/home">
